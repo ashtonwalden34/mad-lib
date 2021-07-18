278778e75d87b4267e39b86a014e6f047fc9eddc
Ashton Walden <ashtonwalden@icloud.com>
2021-07-18 04:02:49 -0600

    attempting to fix error

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-
-
 var noun1 = prompt("Enter a noun");
 var adj1 = prompt("Enter an adjective");
 var pronoun1 = prompt("Enter a pronoun");
@@ -28,38 +26,10 @@ function madLib(noun1, adj1, pronoun1, noun2, adj2, adj3,
     return "Once upon a " + noun1 + " there was a " + adj1 + " " + noun2 + 
         " named Wilbur. " +  pronoun1 + " had a very " + adj2 + " and " + adj3 + " sister named Penelope. " + 
         pronoun2 + " loved to " + verb1 + " and " +  verb2 +  " with " +  pronoun3 + " sister. " + 
-        "They would " + verb3 + " very " + adverb1 + prep1 + " eachother " +  prep2  + " the sun came up. " +  
+        "They would " + verb3 + " very " + adverb1 + " "  + prep1 + " eachother " +  prep2  + " the sun came up. " +  
         "Penelope " + conj1 + " Wilbur would aslo always be " + verb4 + " very " + adverb2 + ". "  + 
         "Penelope would always scream " +  inter1  + "every time she lost, while Wilbur would exclaim " + inter2 + ". " + 
         "Sometimes they would argue whether they should play video games " + conj2 + " watch TV."
 }
 
 // alert (madLib(noun1, adj1, pronoun1, noun2, adj2, adj3, pronoun2, verb1, verb2, pronoun3, verb3, adverb1, prep1, prep2, conj1, verb4, adverb2, inter1, inter2, conj2));
-
-
-
-
-/*
-    Once upon a {noun} there was a {adjective} {noun} named 
-    Wilbur. {Pronoun} had a very {adjective} and {adjective} 
-    sister named Penelope. {Pronoun} loved to 
-    {verb} and {verb} with {pronoun} sister.
-    They would {verb} very {adverb}  {preposition} 
-    eachother {preposition} the sun came up. 
-    Penelope {conjunction} Wilbur would aslo always be 
-    {verb -ing} very {adverb}. Penelope would always scream
-    {interjection} every time she lost while Wilbur would
-    exclaim {interjection}. Sometimes they would argue 
-    whether they should play video games {conjunction} watch TV.
-
-    Once upon a " + noun1 + " there was a " adj1 + " " + noun2 " named Wilbur. pronoun1 had a very adj2 and adj3 
-    sister named Penelope. pronoun2 loved to 
-    verb1 and verb2 with pronoun3 sister.
-    They would verb3 very adverb1 prep1 
-    eachother prep2 the sun came up. 
-    Penelope conj1 Wilbur would aslo always be 
-    verb4 very adverb2. Penelope would always scream
-    inter1 every time she lost while Wilbur would
-    exclaim inter2. Sometimes they would argue 
-    whether they should play video games conj2 watch TV.
-*/
