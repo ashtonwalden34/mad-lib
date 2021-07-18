9e959515efaba6415143774e227a8394d02ead35
Ashton Walden <ashtonwalden@icloud.com>
2021-07-18 03:38:38 -0600

    prompts user, alerts sotry, and shows in html

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # mad-lib
 CSIS 1430 - Module 5 Assignment 2 - Mad Libs Game - Summer Semester 2021
+
+https://ashtonwalden34.github.io/mad-lib/
---
+++ js/script.js
@@ -0,0 +1,65 @@
+
+
+var noun1 = prompt("Enter a noun");
+var adj1 = prompt("Enter an adjective");
+var pronoun1 = prompt("Enter a pronoun");
+var noun2 = prompt("Enter a noun");
+var adj2 = prompt("Enter an adjective");
+var adj3 = prompt("Enter an adjective");
+var pronoun2 = prompt("Enter a pronoun");
+var verb1 = prompt("Enter a verb");
+var verb2 = prompt("Enter a verb");
+var pronoun3 = prompt("Enter a pronoun");
+var verb3 = prompt("Enter a verb");
+var adverb1 = prompt("Enter an adverb");
+var prep1 = prompt("Enter a preposition");
+var prep2 = prompt("Enter a preposition");
+var conj1 = prompt("Enter a conjuction");
+var verb4 = prompt("Enter a verb");
+var adverb2 = prompt("Enter an adverb");
+var inter1 = prompt("Enter an interjection");
+var inter2 = prompt("Enter an interjection");
+var conj2 = prompt("Enter a conjuction");
+
+function madLib(noun1, adj1, pronoun1, noun2, adj2, adj3, 
+    pronoun2, verb1, verb2, pronoun3, verb3, adverb1, prep1, 
+    prep2, conj1, verb4, adverb2, inter1, inter2, conj2) 
+{
+    return "Once upon a " + noun1 + " there was a " + adj1 + " " + noun2 + 
+        " named Wilbur. " +  pronoun1 + " had a very " + adj2 + " and " + adj3 + " sister named Penelope. " + 
+        pronoun2 + " loved to " + verb1 + " and " +  verb2 +  " with " +  pronoun3 + " sister. " + 
+        "They would " + verb3 + " very " + adverb1 + prep1 + " eachother " +  prep2  + " the sun came up. " +  
+        "Penelope " + conj1 + " Wilbur would aslo always be " + verb4 + " very " + adverb2 + ". "  + 
+        "Penelope would always scream " +  inter1  + "every time she lost, while Wilbur would exclaim " + inter2 + ". " + 
+        "Sometimes they would argue whether they should play video games " + conj2 + " watch TV."
+}
+
+alert (madLib(noun1, adj1, pronoun1, noun2, adj2, adj3, pronoun2, verb1, verb2, pronoun3, verb3, adverb1, prep1, prep2, conj1, verb4, adverb2, inter1, inter2, conj2));
+
+
+
+
+/*
+    Once upon a {noun} there was a {adjective} {noun} named 
+    Wilbur. {Pronoun} had a very {adjective} and {adjective} 
+    sister named Penelope. {Pronoun} loved to 
+    {verb} and {verb} with {pronoun} sister.
+    They would {verb} very {adverb}  {preposition} 
+    eachother {preposition} the sun came up. 
+    Penelope {conjunction} Wilbur would aslo always be 
+    {verb -ing} very {adverb}. Penelope would always scream
+    {interjection} every time she lost while Wilbur would
+    exclaim {interjection}. Sometimes they would argue 
+    whether they should play video games {conjunction} watch TV.
+
+    Once upon a " + noun1 + " there was a " adj1 + " " + noun2 " named Wilbur. pronoun1 had a very adj2 and adj3 
+    sister named Penelope. pronoun2 loved to 
+    verb1 and verb2 with pronoun3 sister.
+    They would verb3 very adverb1 prep1 
+    eachother prep2 the sun came up. 
+    Penelope conj1 Wilbur would aslo always be 
+    verb4 very adverb2. Penelope would always scream
+    inter1 every time she lost while Wilbur would
+    exclaim inter2. Sometimes they would argue 
+    whether they should play video games conj2 watch TV.
+*/
